c7417815f565ce7ff5850b375e3746c31970b887
Olena Androsovych <o.androsovych@gmail.com>
2019-07-05 15:36:31 +0200

    Checked for PEP8 requirements

--- classes.py
+++ classes.py
@@ -10,71 +10,71 @@ class Data_OFF():
     using the library request"""
     def __init__(self):
         self.categories = LIST_CATEGORIES
-        
+
     def get_data(self):
         """Method which get the necessary information in certain categories"""
         all_products = []
         self.cat_number = 1
         for category in self.categories:
-            p = {"search_terms":category,"json":1, "page_size":100}
-            r = requests.get("https://fr.openfoodfacts.org/cgi/search.pl", \
-                params = p)
+            p = {"search_terms": category, "json": 1, "page_size": 100}
+            r = requests.get("https://fr.openfoodfacts.org/cgi/search.pl",
+                             params=p)
             i = 0
             for i in range(100):
                 if i < NUM_PRODUCTS:
-                    product = {"name": "", "description": "", "url": "", \
-                        "score": "", "category": "", "store": ""}
+                    product = {"name": "", "description": "", "url": "",
+                               "score": "", "category": "", "store": ""}
                     try:
-                        if (len(r.json()["products"][i]["product_name_fr"]) \
-                            > 2 and
-                            len(r.json()["products"][i]["generic_name_fr"]) \
-                                > 2 and
-                            len(r.json()["products"][i]["url"]) \
-                                > 2 and 
-                            len(r.json()["products"][i]["nutrition_grade_fr"])\
-                                >= 1 and 
+                        if (len(r.json()["products"][i]["product_name_fr"]) >
+                            2 and
+                            len(r.json()["products"][i]["generic_name_fr"]) >
+                            2 and
+                            len(r.json()["products"][i]["url"]) >
+                                2 and
+                            (len(r.json()["products"][i]
+                             ["nutrition_grade_fr"]) >= 1) and
                             len(r.json()["products"][i]["stores"]) > 2):
-                            product["name"] = \
-                                r.json()["products"][i]["product_name_fr"]
-                            product["description"] = \
-                                r.json()["products"][i]["generic_name_fr"]
-                            product["url"] = \
-                                r.json()["products"][i]["url"]
-                            product["store"] = \
-                                r.json()['products'][i]['stores']
-                            product["category"] = self.cat_number
-                            product["score"] = \
-                                r.json()["products"][i]["nutrition_grade_fr"] 
-                            product["store"] = \
-                                r.json()["products"][i]["stores"] 
-                            all_products.append(product)
+                                product["name"] = (r.json()["products"][i]
+                                                   ["product_name_fr"])
+                                product["description"] = \
+                                    r.json()["products"][i]["generic_name_fr"]
+                                product["url"] = \
+                                    r.json()["products"][i]["url"]
+                                product["store"] = \
+                                    r.json()['products'][i]['stores']
+                                product["category"] = self.cat_number
+                                product["score"] = (r.json()["products"][i]
+                                                    ["nutrition_grade_fr"])
+                                product["store"] = \
+                                    r.json()["products"][i]["stores"]
+                                all_products.append(product)
                     except (TypeError):
                         pass
                     except (KeyError):
                         pass
                     i += 1
             self.cat_number += 1
-        self.products = all_products  
+        self.products = all_products
         return(self.products)
-      
+
 
 class Data_PB():
     """Claass defines the data which fills the database Pur Beurre"""
     def __init__(self):
         self.data_off = Data_OFF()
 
-    def connect(self): 
-        """Method which creates la connection with PyMySQL"""       
+    def connect(self):
+        """Method which creates la connection with PyMySQL"""
         self.connection = pymysql.connect(host=HOST,
-                                user=USER,
-                                password=PASSWORD,
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
+                                          user=USER,
+                                          password=PASSWORD,
+                                          charset='utf8mb4',
+                                          cursorclass=pymysql.cursors.DictCursor)
 
     def commit_connection(self):
         """Method which commit the changes with the database Pur Beurre"""
         self.connection.commit()
-            
+
     def close_connection(self):
         """Method which closes the connection with the database Pur Beurre"""
         self.connection.close()
@@ -82,14 +82,14 @@ class Data_PB():
     def create_db(self):
         """Method which creates the database"""
         with self.connection.cursor() as cursor:
-            sql_data = "CREATE DATABASE IF NOT EXISTS Project5;" 
+            sql_data = "CREATE DATABASE IF NOT EXISTS Project5;"
             cursor.execute(sql_data)
             self.commit_connection()
-    
+
     def drop_db(self):
         """Method which deletes the database"""
         with self.connection.cursor() as cursor:
-            sql_data = "DROP DATABASE IF EXISTS Project5;" 
+            sql_data = "DROP DATABASE IF EXISTS Project5;"
             cursor.execute(sql_data)
             self.commit_connection()
 
@@ -102,7 +102,7 @@ class Data_PB():
                         id SMALLINT NOT NULL AUTO_INCREMENT,\
                         name VARCHAR(45) NOT NULL,\
                         PRIMARY KEY (id))\
-                        ENGINE=InnoDB;" 
+                        ENGINE=InnoDB;"
             cursor.execute(sql_data)
             self.commit_connection()
 
@@ -146,13 +146,13 @@ class Data_PB():
                         CONSTRAINT fk_substitute_product2\
                             FOREIGN KEY (healthy_product_id)\
                             REFERENCES product(id))\
-                        ENGINE = InnoDB;" 
+                        ENGINE = InnoDB;"
             cursor.execute(sql_data)
-            self.commit_connection()  
+            self.commit_connection()
 
     def insert_category(self):
-        """Method which creates new records and commits the changes 
-        in the table 'category'"""
+        """Method which creates new records and commits the changes
+in the table 'category'"""
         for category in LIST_CATEGORIES:
             with self.connection.cursor() as cursor:
                 sql_data = "USE Project5;"
@@ -162,8 +162,8 @@ class Data_PB():
         self.commit_connection()
 
     def insert_product(self):
-        """Method which creates new records and commits the changes 
-        in the table 'product'"""
+        """Method which creates new records and commits the changes
+in the table 'product'"""
         for prod in self.data_off.get_data():
             with self.connection.cursor() as cursor:
                 sql_data = "USE Project5;"
@@ -173,9 +173,10 @@ class Data_PB():
                     `url`, `score`, `category_id`, `store`) \
                         VALUES (%s, %s, %s, %s, %s, %s)"
                 try:
-                    cursor.execute(sql_data, (prod['name'], \
-                        prod['description'], prod['url'], \
-                        prod['score'], prod['category'], prod['store'])) 
+                    cursor.execute(sql_data, (prod['name'],
+                                   prod['description'], prod['url'],
+                                   prod['score'], prod['category'],
+                                   prod['store']))
                 except:
                     pass
         self.commit_connection()
@@ -191,25 +192,25 @@ class Data_PB():
                 "SELECT DISTINCT * \
                     FROM `" + table + "` WHERE `" + column + "`=%s"
             cursor.execute(sql_data, (search))
-            if how_many == 1: 
+            if how_many == 1:
                 r = cursor.fetchone()
             else:
                 r = cursor.fetchall()
-        #self.close_connection()
-        return r  
-    
-    def insert_substitute(self, id1 = None, id2 = None):
+        # self.close_connection()
+        return r
+
+    def insert_substitute(self, id1=None, id2=None):
         """Method which fils the table 'substitute'"""
-        #self.connect()
+        # self.connect()
         with self.connection.cursor() as cursor:
             sql_data = "USE Project5;"
             cursor.execute(sql_data)
         with self.connection.cursor() as cursor:
             sql_data = "INSERT INTO `substitute` \
                 (`usual_product_id`, `healthy_product_id`) VALUES (%s, %s)"
-            cursor.execute(sql_data, (id1, id2))    
+            cursor.execute(sql_data, (id1, id2))
         self.commit_connection()
-        #self.close_connection()
+        # self.close_connection()
 
     def sql(self, sql, optional):
         """Method which executes mysql code"""
@@ -219,12 +220,13 @@ class Data_PB():
             cursor.execute(sql_data)
         with self.connection.cursor() as cursor:
             cursor.execute(sql)
-            if optional == "one": 
+            if optional == "one":
                 r = cursor.fetchone()
             elif optional == "all":
                 r = cursor.fetchall()
         self.close_connection()
         return r
+
 
 class Display_data():
     """Claass defines the data which enables the user to interact with DB"""
@@ -250,27 +252,27 @@ class Display_data():
             print("Erreur. Vous avez entré un mauvais caractère'")
             print("Retapez, s'il vous plaît")
             print("")
-    
+
     def choose_option(self):
-        """Method which asks the user to choose the product 
-        that he wants to replace"""
+        """Method which asks the user to choose the product to replace"""
         b = 1
         print("Choisissez une catégorie et entrez son numéro :")
         for category in LIST_CATEGORIES:
             print(str(b) + " : " + category)
             b += 1
-        print("")    
+        print("")
         self.choicecat = \
             int(self.super_input(range(1, len(LIST_CATEGORIES)+1)))
-        self.result = self.data_pb.find_from_table\
-            (self.choicecat, "category_id", "product", NUM_PRODUCTS)
+        self.result = self.data_pb.find_from_table(self.choicecat,
+                                                   "category_id", "product",
+                                                   NUM_PRODUCTS)
         print("")
         print("Sélectionnez le numéro du produit que vous voulez remplacer :")
         b = 1
         self.choices = {}
         for product in self.result:
             self.choices[b] = product
-            print(str(b) + " : " + product['name']+ ", " + product['score'])
+            print(str(b) + " : " + product['name'] + ", " + product['score'])
             b += 1
         print("")
         self.chosenproduct = \
@@ -280,8 +282,8 @@ class Display_data():
         """Method which finds the substitute and displays its description"""
         self.substitute_list = []
         for product in self.result:
-            if self.nutricompare(product['score'],\
-                self.chosenproduct['score']):
+            if self.nutricompare(product['score'],
+                                 self.chosenproduct['score']):
                 self.substitute_list.append(product)
         print("")
         print("Choisissez un substitut et entrez son numéro:")
@@ -296,27 +298,27 @@ class Display_data():
             print("Il n'y a pas de produit plus sain dans cette catégorie ")
             self.substitute = self.chosenproduct
         else:
-            self.substitute = self.choices[int(self.super_input(range(1, \
-                len(self.substitute_list)+1)))]
+            self.substitute = self.choices[int(self.super_input(range(1,
+                                           len(self.substitute_list)+1)))]
         print("")
         print("Vous avez choisi : " + self.substitute['name'])
         print("Description : " + self.substitute['description'])
         print("URL : " + self.substitute['url'])
         print("NutriScore : " + self.substitute['score'])
-        print("Catégorie : " +  self.data_pb.find_from_table\
-            (self.substitute['category_id'], "id", "category", 1)['name'])
+        print("Catégorie : " + self.data_pb.find_from_table
+              (self.substitute['category_id'], "id", "category", 1)['name'])
         print("Où acheter : " + self.substitute['store'])
 
     def add_data_choice(self):
         """Method which asks the user to save a substitute product
         into the list of healthy products"""
         print("")
-        print("Enregistrer le résultat dans votre liste des produits sains ?") 
+        print("Enregistrer le résultat dans votre liste des produits sains ?")
         print("Si oui, tapez 'Y'. Si non, tapez 'N'.")
         print("")
         if self.super_input(["Y", "N"]) == "Y":
-            id1 = str(self.data_pb.find_from_table\
-                (self.chosenproduct['id'], "id", "product", 1)['id'])
+            id1 = str(self.data_pb.find_from_table
+                      (self.chosenproduct['id'], "id", "product", 1)['id'])
             id2 = (str(self.data_pb.find_from_table(self.substitute['id'],
                    "id", "product", 1)['id']))
             self.data_pb.insert_substitute(id1, id2)
--- constants.py
+++ constants.py
@@ -1,4 +1,8 @@
 """Constants of the programme Pur Beurre"""
 
-NUM_PRODUCTS = 5 #Products number in each category
-LIST_CATEGORIES = ("snacks", "petit-déjeuners", "chocolats", "confitures", "chips") #category list
+NUM_PRODUCTS = 5  # Products number in each category
+LIST_CATEGORIES = ("snacks",
+                   "petit-déjeuners",
+                   "chocolats",
+                   "confitures",
+                   "chips")  # category list
